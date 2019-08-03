754909369d6c5de3f12f4a38e663ae02e3c6dcc9
Matthew Daffern <38901216+MatthewDaffern@users.noreply.github.com>
2019-08-02 18:40:26 -0700

    messing around with the functional method of coding in powershell

--- powershell_tests.ps1
+++ powershell_tests.ps1
@@ -1,10 +1,37 @@
 $file_location = out-host 'please enter the file location of your user list'
 $Users = Import-CSV $file_location
 
+# need to convert this all to functions. 
 
 function chr_gen{
     $chr = ([char](get-random -max 128))
     return $chr
+}
+
+function password_creator($length, $chr_randomizer){
+    $password = $null
+    1.. $length | & $chr_randomizer >> $password
+    return $password
+}
+
+
+function map($function, $iterable){
+    $old_iterable = $iterable
+    $new_iterable = @()
+    foreach ($item in $old_iterable){   
+        & $function($item) >> $new_iterable
+    }
+    return $new_iterable
+}
+
+
+#setting functions as variables requires scoping the variable to be a function.
+#$foo1 = $function:foo
+#then it's called with &
+
+function create_user($user){
+    #$user should have every parameter available as an attribute
+
 }
 
 
@@ -24,6 +51,6 @@ foreach ($item in $Users) {
     -DisplayName "$item.Lastname, $item.Firstname" `
     -Department $Department `
     -Path $OU `
-    -AccountPassowrd (convertto-securestring $password -AsPlainText -Force)
+    -AccountPassword (convertto-securestring $password -AsPlainText -Force)
     
 }
